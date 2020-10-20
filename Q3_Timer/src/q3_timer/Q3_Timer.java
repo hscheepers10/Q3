344b2332f0c99a50d674034681b7cd557afcca8b
Heinrich <hscheepers10@gmail.com>
2020-10-20 12:34:43 +0200

    Added project code.

---
+++ Q3_Timer/src/q3_timer/Q3_Timer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q3_timer;
+
+/*
+ * @author User
+ */
+
+public class Q3_Timer {
+
+    /*
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+    }
+    
+}
